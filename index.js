bc898d7ddc58fd3cb3c0b2d39fccb8f5b836035d
jasser <jasserj663@gmail.com>
2026-04-09 15:23:54 +0100

    adding human avatars imgs

--- index.js
+++ index.js
@@ -15,7 +15,32 @@ const banner = document.getElementById("banner");
 const playBtn = document.getElementById("playBtn");
 banner.classList.add("visible");
 
+function moveRight() {
+  gameBoard.style.transform = "translateX(350px)"; /* adjust px as needed */
+}
+
+function moveLeft() {
+  gameBoard.style.transform = "translateX(-350px)";
+}
+const imgs = document.querySelectorAll(".img-wrap");
+
+function showImgs() {
+  imgs.forEach((img, i) => {
+    img.style.transitionDelay = `${i * 100}ms`; // 0ms, 100ms, 200ms...
+    img.classList.add("visible");
+  });
+}
+
+function hideImgs() {
+  imgs.forEach((img, i) => {
+    img.style.transitionDelay = `${i * 80}ms`;
+    img.classList.remove("visible");
+  });
+}
+
 playBtn.addEventListener("click", () => {
   banner.classList.remove("visible");
   playBtn.classList.add("hidden");
+  moveRight();
+  setTimeout(() => showImgs(), 1000);
 });
