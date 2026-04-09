786249ea2b08f701d9ccd67163c778ef9adcc462
jasser <jasserj663@gmail.com>
2026-04-09 21:47:32 +0100

    adding the ui for the red team

--- index.js
+++ index.js
@@ -4,8 +4,10 @@ const ranks = [8, 7, 6, 5, 4, 3, 2, 1];
 const gameBoard = document.getElementById("gameboard");
 const banner = document.getElementById("banner");
 const playBtn = document.getElementById("playBtn");
-const imgs = document.querySelectorAll(".img-wrap");
-const checkBtn = document.getElementById("check-btn");
+const imgsBlue = document.querySelectorAll(".img-wrap-blue");
+const imgsRed = document.querySelectorAll(".img-wrap-red");
+const checkBtnBlue = document.getElementById("check-btn-blue");
+const checkBtnRed = document.getElementById("check-btn-red");
 
 ranks.forEach((rank) => {
   files.forEach((file) => {
@@ -17,16 +19,16 @@ ranks.forEach((rank) => {
 });
 banner.classList.add("visible");
 
-function showImgs() {
+function showImgs(imgs) {
   imgs.forEach((img, i) => {
     img.style.transitionDelay = `${i * 100}ms`;
     img.classList.add("visible");
   });
 }
 
-function hideImgs() {
+function hideImgs(imgs) {
   imgs.forEach((img, i) => {
-    img.style.transitionDelay = `${i * 80}ms`;
+    img.style.transitionDelay = `${i * 100}ms`;
     img.classList.remove("visible");
   });
 }
@@ -39,14 +41,24 @@ function moveBoardLeft() {
   gameBoard.style.transform = "translateX(-350px)";
 }
 
+function moveBoardCenter() {
+  gameBoard.style.transform = "translateX(0)";
+}
+
 playBtn.addEventListener("click", () => {
   banner.classList.remove("visible");
   playBtn.classList.add("hidden");
   moveBoardRight();
-  setTimeout(() => showImgs(), 1000);
+  setTimeout(() => showImgs(imgsBlue), 1000);
 });
 
-checkBtn.addEventListener("click", () => {
-  hideImgs();
-  setTimeout(() => moveBoardLeft(), 1000);
+checkBtnBlue.addEventListener("click", () => {
+  hideImgs(imgsBlue); // 1. Blue fades out
+  setTimeout(() => moveBoardLeft(), 1000); // 2. Board slides left
+  setTimeout(() => showImgs(imgsRed), 2000); // 3. Red appears (after board moves)
+});
+
+checkBtnRed.addEventListener("click", () => {
+  hideImgs(imgsRed); // 1. Blue fades out
+  setTimeout(() => moveBoardCenter(), 1000); // 2. Board slides left
 });
